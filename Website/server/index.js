c6d155198e4b7c7648d305a2f4fa57a56e0e0acf
ugottalovetin <gonzalesjustin1307@gmail.com>
2025-06-04 14:04:12 +0800

    login with database
    not yet done

--- Website/server/index.js
+++ Website/server/index.js
@@ -59,7 +59,7 @@ async function archiveCompletedOrCancelledOrders() {
           ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)`,
           [
             order.order_id,
-            order.name, // Using name as customer_name
+            order.name, 
             order.name,
             order.shipped_to,
             order.order_date,
